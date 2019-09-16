ff2c900b2c2017fedbbf953a058fb8d28a425e4b
Derek <legato_5@yahoo.com>
2019-09-15 20:17:02 -0700

    got basic carousel working and infinite loop. will try to get animations going as well.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,68 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createCarousel() {
+  //Create new Elements
+  const car = document.createElement('div');
+  const left = document.createElement('div');
+  const pic = [document.createElement('img'), 
+  document.createElement('img'), 
+  document.createElement('img'), 
+  document.createElement('img')];
+  const right = document.createElement('div');
+  
+  //add elements to carousel
+  car.appendChild(left);
+  car.appendChild(pic[0]);
+  car.appendChild(pic[1]);
+  car.appendChild(pic[2]);
+  car.appendChild(pic[3]);
+  car.appendChild(right);
+
+  //add classes to elements
+  car.classList.add('carousel');
+  left.classList.add('left-button');
+  right.classList.add('right-button');
+
+  //add content to elements
+  left.textContent = ' < ';
+  right.textContent = ' > ';
+  pic[0].src = "./assets/carousel/mountains.jpeg";
+  pic[1].src = "./assets/carousel/computer.jpeg";
+  pic[2].src = "./assets/carousel/trees.jpeg";
+  pic[3].src = "./assets/carousel/turntable.jpeg";
+  
+  pic[0].style.display = 'block';
+
+  let picNum = 0;
+  right.addEventListener('click', (event) => {
+   pic[picNum].style.display = 'none';
+   if(picNum >= pic.length - 1){
+     picNum = 0;
+   }
+   else {
+    picNum++;
+   }
+   pic[picNum].style.display = 'block';
+  } )
+
+  left.addEventListener('click', (event) => {
+    pic[picNum].style.display = 'none';
+    if(picNum <= 0){
+      picNum = pic.length - 1;
+    }
+    else {
+     picNum--;
+    }
+
+    pic[picNum].style.display = 'block';
+   } )
+
+  //return element
+  return car;
+}
+
+const carContainer = document.querySelector('.carousel-container');
+
+carContainer.appendChild(createCarousel());
